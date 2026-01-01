e3e39fce8173fe2c7f410610c5f3894b8ebc2dc4
Swimnato <66497456+Swimnato@users.noreply.github.com>
2025-12-31 17:33:42 -0700

    St7796S code, however CS is not asserting for some reason, Am going to try other library after this
    Integrated Adafruit_ST7796S library for dual LCD support, initialized two ST7796S displays, and added related pin definitions. Reduced SPI frequency from 80MHz to 30MHz for compatibility. Updated dependencies in platformio.ini to include the Adafruit ST7735 and ST7789 library.

--- Logic_Re-Initialization_Panel_Code/src/main.cpp
+++ Logic_Re-Initialization_Panel_Code/src/main.cpp
@@ -4,27 +4,74 @@
 #include <ESP32Servo.h>
 #include <MainBoard.h>
 #include <isoCards.h>
-#include <Adafruit_ILI9341.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ST7796S.h>
 #include <Wire.h>
 #include <Ethernet3.h>
 #include <SdFat.h>
 #include "cardParser.h"
+#include <Adafruit_ILI9341.h>
 
 #define LOGIC_GATES_W 4
 #define LOGIC_GATES_H 4
 #define LOGIC_GATES_PORTS_USED 4
 
+#define LCD_H 320
+#define LCD_W 480
+
 const uint8_t portsUsedForGates[LOGIC_GATES_PORTS_USED] = {MAIN_BOARD_ANALOG_PORT_1, MAIN_BOARD_ANALOG_PORT_2, MAIN_BOARD_ANALOG_PORT_3, MAIN_BOARD_ANALOG_PORT_4};
 
 #define LED_COUNT 20
 
-const uint8_t pinDemux[4][3] = {{0x2,0x4,0x0}, {0x3,0x1,0x5}, {0x9,0x7,0xB}, {0xA, 0x8, 0x6}};
+extern SPIClass mainBoardSpi;
+
+Adafruit_ST7796S display1(&mainBoardSpi, MAIN_BOARD_LCD_1_CS, MAIN_BOARD_LCD_DC, MAIN_BOARD_LCD_RESET);
+Adafruit_ST7796S display2(&mainBoardSpi, MAIN_BOARD_LCD_2_CS, MAIN_BOARD_LCD_DC, MAIN_BOARD_LCD_RESET);
+// Adafruit_ILI9341 disp(&mainBoardSpi, MAIN_BOARD_LCD_2_CS, MAIN_BOARD_LCD_DC, MAIN_BOARD_LCD_RESET);
+
+void showScreenDebug(LOGIC_CARD* readValues,uint16_t parsed_W,uint16_t parsed_H){
+  
+}
 
 void setup() {
   MainBoardStart(false);
   Serial.println("Logic Re-Initialization Panel");
 
   setupParseLogicCards(LOGIC_GATES_W, LOGIC_GATES_H, portsUsedForGates ,LOGIC_GATES_PORTS_USED);
+
+  pinMode(MAIN_BOARD_LCD_1_CS, INPUT_PULLUP);
+  pinMode(MAIN_BOARD_LCD_2_CS, INPUT_PULLUP);
+
+  analogWrite(MAIN_BOARD_LCD_BRIGHTNESS, 1024);
+  analogWrite(MAIN_BOARD_LCD_RESET, 0);
+  delay(10);
+  analogWrite(MAIN_BOARD_LCD_RESET, 1024);
+  display1.init(320, 480, 0, 0, ST7796S_BGR);
+  display1.invertDisplay(true);
+  display1.fillScreen(display1.color565(255,0,0));
+  display2.init(320, 480, 0, 0, ST7796S_BGR);
+  display2.invertDisplay(true);
+  display2.fillScreen(display2.color565(255,0,0));
+  delay(250);
+  display1.fillScreen(display1.color565(0,255,0));
+  unsigned long startFill = millis();
+  display2.fillScreen(display2.color565(0,255,0));
+  Serial.printf("Screen fill time: %d\n", millis() - startFill);
+  delay(250);
+  display1.fillScreen(display1.color565(0,0,255));
+  display2.fillScreen(display2.color565(0,0,255));
+  delay(250);
+  display1.fillScreen(0);
+  display2.fillScreen(0);
+  display1.setCursor(0,0);
+  display2.setCursor(0,0);
+  display1.setTextColor(0xFFFF);
+  display2.setTextColor(0xFFFF);
+  display1.print("Screen ONE initialized");
+  display2.print("Screen TWO initialized");
+
+  // initISOCards(8);
+  // addISOCard(0x01,)
 
   Serial.print("Initialization Complete @");
   Serial.print(millis());
